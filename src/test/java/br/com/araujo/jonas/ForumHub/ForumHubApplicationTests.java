19a02f89e90aa39104cd734197e7a74ef82f659f
Jonas Araujo <jonaslaraujo@gmail.com>
2024-07-15 23:12:51 -0300

    Commit inicial

---
+++ src/test/java/br/com/araujo/jonas/ForumHub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.araujo.jonas.ForumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
